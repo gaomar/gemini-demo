4fe19fd2459116c14161098b2e02268c8205bcdc
h-takauma <h-takauma@i-enter.co.jp>
2023-12-14 16:56:25 +0900

    prettier追加

--- chat.ts
+++ chat.ts
@@ -2,8 +2,8 @@ import { GoogleGenerativeAI } from '@google/generative-ai'
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY as string)
 
-async function main() {
-  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+async function run() {
+  const model = genAI.getGenerativeModel({ model: 'gemini-pro' })
   const prompt = `
   # 目的
   あなたは優秀なタイトルデザイナーです。
@@ -13,10 +13,10 @@ async function main() {
   生成AIについて
   `
 
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
+  const result = await model.generateContent(prompt)
+  const response = await result.response
+  const text = response.text()
+  console.log(text)
 }
 
-main()
+run()
--- vision.ts
+++ vision.ts
@@ -1,18 +1,17 @@
-import { GoogleGenerativeAI, Part } from "@google/generative-ai";
-import * as fs from "fs";
+import { GoogleGenerativeAI, Part } from '@google/generative-ai'
+import * as fs from 'fs'
 
-const genAI = new GoogleGenerativeAI(process.env.API_KEY as string);
+const genAI = new GoogleGenerativeAI(process.env.API_KEY as string)
 
 // URLからbase64エンコードされた文字列を取得します。
 async function urlToGenerativePart(url: string, mimeType: string): Promise<Part> {
-  const response = await fetch(url);
-  const arrayBuffer = await response.arrayBuffer();
+  const response = await fetch(url)
+  const arrayBuffer = await response.arrayBuffer()
   return {
     inlineData: {
       data: Buffer.from(arrayBuffer).toString('base64'),
-      mimeType
-    }
-
+      mimeType,
+    },
   }
 }
 
@@ -20,27 +19,30 @@ async function urlToGenerativePart(url: string, mimeType: string): Promise<Part>
 function fileToGenerativePart(path: string, mimeType: string): Part {
   return {
     inlineData: {
-      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
-      mimeType
+      data: Buffer.from(fs.readFileSync(path)).toString('base64'),
+      mimeType,
     },
-  };
+  }
 }
 
 async function run() {
   // テキストと画像の入力（マルチモーダル）には、gemini-pro-vision モデルを使用します
-  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+  const model = genAI.getGenerativeModel({ model: 'gemini-pro-vision' })
 
-  const prompt = "何が写っている？";
+  const prompt = '何が写っている？'
 
   const imageParts: Part[] = [
-    fileToGenerativePart("cat.png", "image/png"),
-    await urlToGenerativePart("https://1.bp.blogspot.com/-tVeC6En4e_E/X96mhDTzJNI/AAAAAAABdBo/jlD_jvZvMuk3qUcNjA_XORrA4w3lhPkdQCNcBGAsYHQ/s1048/onepiece01_luffy.png", "image/png")
-  ];
+    fileToGenerativePart('cat.png', 'image/png'),
+    await urlToGenerativePart(
+      'https://1.bp.blogspot.com/-tVeC6En4e_E/X96mhDTzJNI/AAAAAAABdBo/jlD_jvZvMuk3qUcNjA_XORrA4w3lhPkdQCNcBGAsYHQ/s1048/onepiece01_luffy.png',
+      'image/png'
+    ),
+  ]
 
-  const result = await model.generateContent([prompt, ...imageParts]);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
+  const result = await model.generateContent([prompt, ...imageParts])
+  const response = await result.response
+  const text = response.text()
+  console.log(text)
 }
 
-run();
+run()
